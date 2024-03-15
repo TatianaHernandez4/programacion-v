dd61b34b5e8ec22b5066251f3856488a0934630b
TatianaHernández4 <tatiana.hernandezposada@gmail.com>
2024-03-14 21:00:22 -0500

    message

---
+++ intro/operadores.js
@@ -0,0 +1,6 @@
+let counter = 100;
+
+counter = counter +20;
+console.log(counter);
+counter +=20;
+console.log(counter);
---
+++ intro/variables.js
@@ -0,0 +1,9 @@
+var brand = "mala-practica";
+let counter = 100;
+const token = "abc123";
+
+console.log(token);
+counter = 200;
+console.log(counter);
+
+//comentario de una linea 
